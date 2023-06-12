442f69f95553aa13babc7a8ee6509d2d974ab5e9
Mateo Encalada <mateo.encalada@udla.edu.ec>
2023-06-11 21:56:19 -0500

    CRUD

---
+++ Models/InformacionME_FV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoP2ME.FV.Models
+{
+    public class InformacionME_FV
+    {
+        public string Semestre { get; set; }
+        public string Materia { get; set; }
+        public string NombreProfesor { get; set; }
+        public string Calificacion { get; set; }
+        public string Descripcion { get; set; }
+        public string Cualidad { get; set; }
+        public string Horario { get; set; }
+    }
+}
---
+++ PrincipalME_FV.xaml.cs
@@ -0,0 +1,102 @@
+using Microsoft.VisualBasic;
+using ProyectoP2ME.FV.Models;
+using System.Collections.ObjectModel;
+
+namespace ProyectoP2ME.FV;
+
+public partial class PrincipalME_FV : ContentPage
+{ 
+ List<InformacionME_FV> informationList = new List<InformacionME_FV>();
+ObservableCollection<InformacionME_FV> displayedInformationList = new ObservableCollection<InformacionME_FV>();
+
+public PrincipalME_FV()
+{
+    InitializeComponent();
+    infoListView.ItemsSource = displayedInformationList;
+}
+
+private void AddToTable(object sender, EventArgs e)
+{
+    InformacionME_FV information = new InformacionME_FV
+    {
+        Semestre = semesterEntry.Text,
+        Materia = subjectEntry.Text,
+        NombreProfesor = professorEntry.Text,
+        Calificacion = ratingEntry.Text,
+        Descripcion = descriptionEntry.Text,
+        Cualidad = qualityEntry.Text,
+        Horario = scheduleEntry.Text
+    };
+
+    informationList.Add(information);
+    displayedInformationList.Add(information);
+
+    ClearEntries();
+}
+
+private void Search(object sender, EventArgs e)
+{
+    string searchKeyword = searchEntry.Text.ToLower();
+
+    displayedInformationList.Clear();
+
+    foreach (var information in informationList)
+    {
+        if (information.Materia.ToLower().Contains(searchKeyword))
+        {
+            displayedInformationList.Add(information);
+        }
+    }
+}
+
+private async void DeleteItem(object sender, EventArgs e)
+{
+    if (sender is Button button && button.CommandParameter is InformacionME_FV information)
+    {
+        await DeleteInformation(information);
+    }
+}
+
+private async void ModifyItem(object sender, EventArgs e)
+{
+    if (sender is Button button && button.CommandParameter is InformacionME_FV information)
+    {
+        await ModifyInformation(information);
+    }
+}
+
+private async Task DeleteInformation(InformacionME_FV information)
+{
+    await Task.Delay(100); // Simulating an asynchronous operation
+    informationList.Remove(information);
+    displayedInformationList.Remove(information);
+}
+
+private async Task ModifyInformation(InformacionME_FV information)
+{
+    await Task.Delay(100); // Simulating an asynchronous operation
+                           // Set the information values to the entry fields
+    semesterEntry.Text = information.Semestre;
+    subjectEntry.Text = information.Materia;
+    professorEntry.Text = information.NombreProfesor;
+    ratingEntry.Text = information.Calificacion;
+    descriptionEntry.Text = information.Descripcion;
+    qualityEntry.Text = information.Cualidad;
+    scheduleEntry.Text = information.Horario;
+
+    // Remove the information from the list temporarily for modification
+    informationList.Remove(information);
+    displayedInformationList.Remove(information);
+}
+
+private void ClearEntries()
+{
+    semesterEntry.Text = string.Empty;
+    subjectEntry.Text = string.Empty;
+    professorEntry.Text = string.Empty;
+    ratingEntry.Text = string.Empty;
+    descriptionEntry.Text = string.Empty;
+    qualityEntry.Text = string.Empty;
+    scheduleEntry.Text = string.Empty;
+}
+    }
