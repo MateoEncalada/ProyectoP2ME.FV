5a8049b0cfe100bb0b31ba6fffbc973fa968f6a0
Mateo Encalada <mateo.encalada@udla.edu.ec>
2023-07-15 12:41:27 -0500

    Creacion Repoitorio de informacion para el SQL

---
+++ InfoRepository.cs
@@ -0,0 +1,73 @@
+﻿using ProyectoP2ME.FV.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoP2ME.FV
+{
+    public class InfoRepository
+    {
+        string _dbPath;
+        public string StatusMessage { get; set; }
+        private SQLiteAsyncConnection conn;
+        private async Task Init()
+        {
+            if (conn != null)
+                return;
+
+            conn = new SQLiteAsyncConnection(_dbPath);
+
+            await conn.CreateTableAsync<InformacionME_FV>();
+        }
+        public InfoRepository (string dbPath)
+        { 
+            _dbPath = dbPath; 
+        }
+
+        public async Task AddNewInfo(
+            int NuevaInfoId, string Semestre, string Materia, string NProfesor, string Calificacion, string Descripcion, string Cualidad, string Horario)
+        {
+            int result = 0;
+            try
+            {
+                await Init();
+
+                // basic validation to ensure a name was entered
+                if (string.IsNullOrEmpty(NuevaInfoId.ToString()))
+                    throw new Exception("Valid name required");
+                
+
+                result = await conn.InsertAsync(new InformacionME_FV { IdInfo = NuevaInfoId, Materia = Materia, NombreProfesor = NProfesor,
+                Calificacion = Calificacion, Descripcion = Descripcion, Cualidad = Cualidad, Horario = Horario, });
+
+
+                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, NuevaInfoId);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to add {0}. Error: {1}", NuevaInfoId, ex.Message);
+            }
+
+        }
+
+        public async Task<List<InformacionME_FV>> GetAllPeople()
+        {
+            // TODO: Init then retrieve a list of Person objects from the database into a list
+            try
+            {
+                await Init();
+                return await conn.Table<InformacionME_FV>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            return new List<InformacionME_FV>();
+        }
+
+    }
+}
--- Models/InformacionME_FV.cs
+++ Models/InformacionME_FV.cs
@@ -5,21 +5,30 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ColumnAttribute = SQLite.ColumnAttribute;
+using TableAttribute = SQLite.TableAttribute;
 
 namespace ProyectoP2ME.FV.Models
 {
-    //[Table("Informacion")]
+    [Table("Informacion")]
     public class InformacionME_FV
     {
-        //[PrimaryKey, AutoIncrement]
+        [PrimaryKey, AutoIncrement,NotNull,Column("Id")]
         public int ID { get; set; }
-        //[Column("Semestre")]
+        public int IdInfo { get; set; }
+        [Column("Semestre"), Indexed,NotNull]
         public string Semestre { get; set; }
+        [NotNull]
         public string Materia { get; set; }
+        [NotNull]
         public string NombreProfesor { get; set; }
+        [NotNull]
         public string Calificacion { get; set; }
+        [MaxLength(100)]
         public string Descripcion { get; set; }
+        [NotNull]
         public string Cualidad { get; set; }
+        [NotNull]
         public string Horario { get; set; }
     }
 }
